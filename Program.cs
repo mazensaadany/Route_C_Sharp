01bd375dde6c4e1b5f158500190bb9b3b48b3d4f
mazensaadany <mazenelsaadany3@gmail.com>
2026-09-09 20:29:29 +0300

    13th ans

--- Program.cs
+++ Program.cs
@@ -146,7 +146,7 @@ namespace RouteAss_2;
 
         string[] books = { "Clean Code", "Refactoring", "The Pragmatic Programmer" };
 
-        for(int i = 0; i < books.Length; i++)
+        for (int i = 0; i < books.Length; i++)
         {
             Console.WriteLine($"{i+1}.{books[i]}");
         }
