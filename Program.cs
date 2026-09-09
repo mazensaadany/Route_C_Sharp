10d29aa293672c8f6ce400adee22820fedeed4b8
mazensaadany <mazenelsaadany3@gmail.com>
2026-09-09 16:32:09 +0300

    1st question answered

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace RouteAss_2
-{
+﻿namespace RouteAss_2;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+        #region  [1st Answer]
+
+        string title = "clean code";
+
+
+        string upperTitle = title.ToUpper();
+
+        Console.WriteLine(title);
+        Console.WriteLine(upperTitle);
+
+        #endregion
+
+        
+
+
     }
 }
+
